5086fae4370580587d80f10d3d62aa761627cccd
Ryan Cheung <110103899+H2ODERP@users.noreply.github.com>
2024-01-21 16:26:07 -0500

    Update AbbreviationExpander.plugin.js
    Updated

--- AbbreviationExpander.plugin.js
+++ AbbreviationExpander.plugin.js
@@ -1,100 +1,63 @@
+//META{"name":"test plugin"}*//
+
 /**
  * @name AbbreviationExpander
  * @author RyanEthanTao
- * @version 1.0.0
- * @description Expands abbreviations
+ * @description Automatically expands abbreviations into their expanded form
+ * @version 1.0
  */
 
-module.exports = (_ => {
-    const changeLog = {};
+const myCallback = mutations => {
+	for(let mutation of mutations){
+	// We only care about our button being removed
+	// Convert to array to allow array functions
+	const addedNodes = Array.from(mutation.addedNodes);
+	for(let addedNode of addedNodes){
+		console.log(typeof addedNode);
+	processingStuff2(addedNode);
+}
+}
+};
+const myObserver = new MutationObserver(myCallback);
+const observerOptions = {
+	childList: true,
+	subtree: true 
+};
+const replacements = [
+	["brb","be right back"],
+	["lol", "laugh out loud"],
+]
+// function processingStuff() {
+// 	for (let randomVar of document.body.getElementsByTagName("span")){
+// 		if(randomVar.childElementCount==0)
+// 			randomVar.innerHTML = randomVar.innerHTML.replace("brb","be right back");
+// 	}
+// }
+function processingStuff2(addedNode) {
+	for (let childElement of addedNode.children){
+		if(childElement.childElementCount==0){
+			//childElement.innerHTML = childElement.innerHTML.replace("brb","be right back");
+			for(let replacementPair of replacements){
+				childElement.innerHTML = childElement.innerHTML.replace(replacementPair[0],replacementPair[1]);
+			}
+		}
+		else{
+			processingStuff2(childElement);
+		}
+	}
+}
+module.exports = class testing {
 
-    return !window.BDFDB_Global || (!window.BDFDB_Global.loaded && !window.BDFDB_Global.started) ? class {
-        constructor(meta) { for (let key in meta) this[key] = meta[key]; }
-        getName() { return this.name; }
-        getAuthor() { return this.author; }
-        getVersion() { return this.version; }
-        getDescription() { return `The Library Plugin needed for ${this.name} is missing. Open the Plugin Settings to download it. \n\n${this.description}`; }
+    start() {
+		BdApi.alert("Hello World!", "This is my first plugin!");
+		processingStuff2(document.body);
+		myObserver.observe(document.body, observerOptions);
+      // Called when the plugin is activated (including after reloads)
+    } 
 
-        downloadLibrary() {
-            require("request").get("https://mwittrien.github.io/BetterDiscordAddons/Library/0BDFDB.plugin.js", (e, r, b) => {
-                if (!e && b && r.statusCode == 200) require("fs").writeFile(require("path").join(BdApi.Plugins.folder, "0BDFDB.plugin.js"), b, _ => BdApi.showToast("Finished downloading BDFDB Library", { type: "success" }));
-                else BdApi.alert("Error", "Could not download BDFDB Library Plugin. Try again later or download it manually from GitHub: https://mwittrien.github.io/downloader/?library");
-            });
-        }
+    stop() {
+		myObserver.disconnect();
+      // Called when the plugin is deactivated
+    } 
+}
 
-        load() {
-            if (!window.BDFDB_Global || !Array.isArray(window.BDFDB_Global.pluginQueue)) window.BDFDB_Global = Object.assign({}, window.BDFDB_Global, { pluginQueue: [] });
-            if (!window.BDFDB_Global.downloadModal) {
-                window.BDFDB_Global.downloadModal = true;
-                BdApi.showConfirmationModal("Library Missing", `The Library Plugin needed for ${this.name} is missing. Please click "Download Now" to install it.`, {
-                    confirmText: "Download Now",
-                    cancelText: "Cancel",
-                    onCancel: _ => { delete window.BDFDB_Global.downloadModal; },
-                    onConfirm: _ => {
-                        delete window.BDFDB_Global.downloadModal;
-                        this.downloadLibrary();
-                    }
-                });
-            }
-            if (!window.BDFDB_Global.pluginQueue.includes(this.name)) window.BDFDB_Global.pluginQueue.push(this.name);
-        }
-        start() { this.load(); }
-        stop() { }
-        getSettingsPanel() {
-            let template = document.createElement("template");
-            template.innerHTML = `<div style="color: var(--header-primary); font-size: 16px; font-weight: 300; white-space: pre; line-height: 22px;">The Library Plugin needed for ${this.name} is missing.\nPlease click <a style="font-weight: 500;">Download Now</a> to install it.</div>`;
-            template.content.firstElementChild.querySelector("a").addEventListener("click", this.downloadLibrary);
-            return template.content.firstElementChild;
-        }
-    } : (([Plugin, BDFDB]) => {
-        const abbreviations = {
-            'brb': 'be right back',
-            'omg': 'oh my god',
-            'lol': 'laugh out loud',
-            // Add more abbreviations and their expanded forms as needed
-        };
-
-        return class AbbreviationExpander extends Plugin {
-            onLoad() {
-                this.modulePatches = {
-                    before: [
-                        "Message",
-                        "MessageContent",
-                    ],
-                };
-            }
-
-            onStart() {
-                this.forceUpdateAll();
-            }
-
-            onStop() {
-                this.forceUpdateAll();
-            }
-
-            processMessageContent(e) {
-                if (!e.instance.props.message) return;
-
-                const expandedMessage = this.expandAbbreviations(e.instance.props.message.content);
-                if (expandedMessage !== e.instance.props.message.content) {
-                    e.instance.props.message.content = expandedMessage;
-                }
-            }
-
-            expandAbbreviations(message) {
-                for (const abbreviation in abbreviations) {
-                    if (abbreviations.hasOwnProperty(abbreviation)) {
-                        const regex = new RegExp(`\\b${abbreviation}\\b`, 'gi');
-                        message = message.replace(regex, abbreviations[abbreviation]);
-                    }
-                }
-                return message;
-            }
-
-            forceUpdateAll() {
-                BDFDB.PatchUtils.forceAllUpdates(this);
-                BDFDB.MessageUtils.rerenderAll();
-            }
-        };
-    })(window.BDFDB_Global.PluginUtils.buildPlugin(changeLog));
-})();
